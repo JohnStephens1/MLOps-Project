8967a2dfaf8047dfb6817f7f50938952210ad80d
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-24 05:34:07 +0000

    WIP: Refactor: comment out deprecated functions and sort new ones

--- src/text_classifier/data/model.py
+++ src/text_classifier/data/model.py
@@ -4,28 +4,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from xgboost import XGBClassifier
 from text_classifier.data.data import data_pipeline, get_raw_dataset
+from text_classifier.model.model import get_model_pipe
 
 
-def get_X_y(
-    df: pd.DataFrame, target_col: str = "tag"
-) -> tuple[pd.DataFrame, pd.Series]:
-    """Splits target_col off of df, then returns the remaining dataset and the isolated column
-
-    Args:
-        df (pd.DataFrame): df
-        target_col (str, optional): the name of the column to be split off. Defaults to "tag".
-
-    Returns:
-        tuple[pd.DataFrame, pd.Series]: the remaining dataset and the isolated column
-    """
-    X = df.drop(target_col, axis=1)
-    y = df[target_col]
-
-    return X, y
-
+# OLD
 
 # def get_train_test_df(
 #     df: pd.DataFrame, target_col: str = "tag", test_size: float = 0.2, seed: int = 1234
@@ -50,59 +33,49 @@ def get_X_y(
 #     return X_train, X_test, y_train, y_test
 
 
-def get_train_test_df(
-    X: pd.DataFrame, y: np.typing.ArrayLike, test_size: float = 0.2, seed: int = 1234
-) -> tuple[pd.DataFrame, pd.DataFrame, np.typing.ArrayLike, np.typing.ArrayLike]:
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, stratify=y, test_size=test_size, random_state=seed
-    )
+# def get_scaled_target(
+#     X_train: pd.DataFrame,
+#     X_test: pd.DataFrame,
+#     range: tuple[int, int] = (0, 1),
+#     target_col: str = "days_since_start",
+# ) -> tuple[MinMaxScaler, pd.DataFrame, pd.DataFrame]:
+#     """scales the target_col in input dataframes via MinMaxScaling
 
-    return X_train, X_test, y_train, y_test
+#     Args:
+#         X_train (pd.DataFrame): X_train df
+#         X_test (pd.DataFrame): X_test df
+#         range (tuple[int, int], optional): the range of the scaler. Defaults to (0, 1).
+#         target_col (str, optional): the name of the target column. Defaults to "days_since_start".
 
+#     Returns:
+#         tuple[MinMaxScaler, pd.DataFrame, pd.DataFrame]: scaler, X_train, X_test
+#     """
+#     scaler = MinMaxScaler(range)
 
-def get_scaled_target(
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    range: tuple[int, int] = (0, 1),
-    target_col: str = "days_since_start",
-) -> tuple[MinMaxScaler, pd.DataFrame, pd.DataFrame]:
-    """scales the target_col in input dataframes via MinMaxScaling
+#     X_train[target_col] = scaler.fit_transform(X_train[[target_col]])
+#     X_test[target_col] = scaler.transform(X_test[[target_col]])
 
-    Args:
-        X_train (pd.DataFrame): X_train df
-        X_test (pd.DataFrame): X_test df
-        range (tuple[int, int], optional): the range of the scaler. Defaults to (0, 1).
-        target_col (str, optional): the name of the target column. Defaults to "days_since_start".
-
-    Returns:
-        tuple[MinMaxScaler, pd.DataFrame, pd.DataFrame]: scaler, X_train, X_test
-    """
-    scaler = MinMaxScaler(range)
-
-    X_train[target_col] = scaler.fit_transform(X_train[[target_col]])
-    X_test[target_col] = scaler.transform(X_test[[target_col]])
-
-    return scaler, X_train, X_test
+#     return scaler, X_train, X_test
 
 
-def get_encoded_target(
-    y_train: np.typing.ArrayLike, y_test: np.typing.ArrayLike
-) -> tuple[LabelEncoder, np.typing.ArrayLike, np.typing.ArrayLike]:
-    """encodes y_train, y_test via LabelEncoder
+# def get_encoded_target(
+#     y_train: np.typing.ArrayLike, y_test: np.typing.ArrayLike
+# ) -> tuple[LabelEncoder, np.typing.ArrayLike, np.typing.ArrayLike]:
+#     """encodes y_train, y_test via LabelEncoder
 
-    Args:
-        y_train (np.typing.ArrayLike): y_train array
-        y_test (np.typing.ArrayLike): y_test array
+#     Args:
+#         y_train (np.typing.ArrayLike): y_train array
+#         y_test (np.typing.ArrayLike): y_test array
 
-    Returns:
-        tuple[LabelEncoder, np.typing.ArrayLike, np.typing.ArrayLike]: encoder, y_train, y_test
-    """
-    encoder = LabelEncoder()
+#     Returns:
+#         tuple[LabelEncoder, np.typing.ArrayLike, np.typing.ArrayLike]: encoder, y_train, y_test
+#     """
+#     encoder = LabelEncoder()
 
-    y_train = encoder.fit_transform(y_train)
-    y_test = encoder.transform(y_test)
+#     y_train = encoder.fit_transform(y_train)
+#     y_test = encoder.transform(y_test)
 
-    return encoder, y_train, y_test
+#     return encoder, y_train, y_test
 
 
 # class PreparedData(TypedDict):
@@ -114,45 +87,48 @@ def get_encoded_target(
 #     y_test: np.typing.ArrayLike
 
 
-def get_model_data(
-    df: pd.DataFrame = get_raw_dataset(),
-    time_col: str = "created_on",
-    target_col: str = "tag",
-) -> tuple[
-    MinMaxScaler,
-    LabelEncoder,
-    pd.DataFrame,
-    pd.DataFrame,
-    np.typing.ArrayLike,
-    np.typing.ArrayLike,
-]:
-    """gets the data for model training and eval, as well as the corresponding scaler and encoder
+# def get_model_data(
+#     df: pd.DataFrame = get_raw_dataset(),
+#     time_col: str = "created_on",
+#     target_col: str = "tag",
+# ) -> tuple[
+#     MinMaxScaler,
+#     LabelEncoder,
+#     pd.DataFrame,
+#     pd.DataFrame,
+#     np.typing.ArrayLike,
+#     np.typing.ArrayLike,
+# ]:
+#     """gets the data for model training and eval, as well as the corresponding scaler and encoder
 
-    Args:
-        df (pd.DataFrame, optional): raw df. Defaults to get_raw_dataset().
-        time_col (str, optional): the name of the time column. Defaults to "created_on".
-        target_col (str, optional): the name of the target column. Defaults to "tag".
+#     Args:
+#         df (pd.DataFrame, optional): raw df. Defaults to get_raw_dataset().
+#         time_col (str, optional): the name of the time column. Defaults to "created_on".
+#         target_col (str, optional): the name of the target column. Defaults to "tag".
 
-    Returns:
-        tuple[MinMaxScaler, LabelEncoder, pd.DataFrame, pd.DataFrame, np.typing.ArrayLike, np.typing.ArrayLike]: scaler, encoder, X_train, X_test, y_train, y_test
-    """
-    df = data_pipeline(df, time_col)
+#     Returns:
+#         tuple[MinMaxScaler, LabelEncoder, pd.DataFrame, pd.DataFrame, np.typing.ArrayLike, np.typing.ArrayLike]: scaler, encoder, X_train, X_test, y_train, y_test
+#     """
+#     df = data_pipeline(df, time_col)
 
-    df = df.drop([time_col, "title", "description", "text"], axis=1)
+#     df = df.drop([time_col, "title", "description", "text"], axis=1)
 
-    X_train, X_test, y_train, y_test = get_train_test_df(df, target_col)
-    scaler, X_train, X_test = get_scaled_target(X_train, X_test)
-    encoder, y_train, y_test = get_encoded_target(y_train, y_test)
+#     X_train, X_test, y_train, y_test = get_train_test_df(df, target_col)
+#     scaler, X_train, X_test = get_scaled_target(X_train, X_test)
+#     encoder, y_train, y_test = get_encoded_target(y_train, y_test)
 
-    return scaler, encoder, X_train, X_test, y_train, y_test
-    # return {
-    #     "scaler": scaler,
-    #     "encoder": encoder,
-    #     "X_train": X_train,
-    #     "X_test": X_test,
-    #     "y_train": y_train,
-    #     "y_test": y_test,
-    # }
+#     return scaler, encoder, X_train, X_test, y_train, y_test
+# return {
+#     "scaler": scaler,
+#     "encoder": encoder,
+#     "X_train": X_train,
+#     "X_test": X_test,
+#     "y_train": y_train,
+#     "y_test": y_test,
+# }
+
+
+# NEW
 
 
 def get_pre_pipe_model_data():
@@ -163,11 +139,39 @@ def get_pre_pipe_model_data():
     return df
 
 
+def get_X_y(
+    df: pd.DataFrame, target_col: str = "tag"
+) -> tuple[pd.DataFrame, pd.Series]:
+    """Splits target_col off of df, then returns the remaining dataset and the isolated column
+
+    Args:
+        df (pd.DataFrame): df
+        target_col (str, optional): the name of the column to be split off. Defaults to "tag".
+
+    Returns:
+        tuple[pd.DataFrame, pd.Series]: the remaining dataset and the isolated column
+    """
+    X = df.drop(target_col, axis=1)
+    y = df[target_col]
+
+    return X, y
+
+
 def get_encoded_y(y: np.typing.ArrayLike) -> tuple[LabelEncoder, np.typing.ArrayLike]:
     label_encoder = LabelEncoder()
     y_encoded = label_encoder.fit_transform(y)
 
     return label_encoder, y_encoded
+
+
+def get_train_test_df(
+    X: pd.DataFrame, y: np.typing.ArrayLike, test_size: float = 0.2, seed: int = 1234
+) -> tuple[pd.DataFrame, pd.DataFrame, np.typing.ArrayLike, np.typing.ArrayLike]:
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, stratify=y, test_size=test_size, random_state=seed
+    )
+
+    return X_train, X_test, y_train, y_test
 
 
 def get_preprocessor(target_cols: list[str] = ["days_since_start"]):
@@ -190,23 +194,7 @@ def nother_df_fn():
     pipe = get_model_pipe()
 
 
-    pass
-
-
-def smth_smth():
-    pass
-    # pre pipe
-    # separate into X, y
-    # apply label encoder to y
-
-
 # post pipe
 
-# label encoder somewhere
-# pipe something
 # potentially store encoder with pipe in dict
 # "joblib to dump model"
-
-
-# notes
-# params via
