764df910ce62b212bba7e6fb8edcff20134439f6
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-24 07:52:15 +0000

    chore: add Dataset, Artifacts, TrainingContext dataclasses

---
+++ src/text_classifier/schema.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder
+
+
+@dataclass
+class Dataset:
+    X_train: pd.DataFrame
+    X_test: pd.DataFrame
+    y_train: np.typing.ArrayLike
+    y_test: np.typing.ArrayLike
+
+
+@dataclass
+class Artifacts:
+    pipe: Pipeline
+    label_encoder: LabelEncoder
+
+
+@dataclass
+class TrainingContext:
+    dataset: Dataset
+    artifacts: Artifacts
