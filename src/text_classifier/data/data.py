568f75c586bc041416c2d746c9b388c79ca19cb8
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-24 06:28:36 +0000

    refactor: update default params and config import in data.py

--- src/text_classifier/data/data.py
+++ src/text_classifier/data/data.py
@@ -3,10 +3,10 @@ import pandas as pd
 from pathlib import Path
 from text_classifier.data.features import add_features
 from text_classifier.data.preprocessing import preprocess_data
-from text_classifier.config import DATASET_PATH
+from text_classifier import config
 
 
-def get_raw_dataset(ds_path: Path = DATASET_PATH) -> pd.DataFrame:
+def get_raw_dataset(ds_path: Path | None = None) -> pd.DataFrame:
     """gets the raw dataset
 
     Args:
@@ -15,23 +15,22 @@ def get_raw_dataset(ds_path: Path = DATASET_PATH) -> pd.DataFrame:
     Returns:
         pd.DataFrame: the loaded df
     """
+    if ds_path is None:
+        ds_path = config.DATASET_PATH
+    
     return pd.read_csv(ds_path)
 
 
-def data_pipeline(
-    df: pd.DataFrame = get_raw_dataset(), time_col: str = "created_on"
-) -> pd.DataFrame:
+def data_pipeline() -> pd.DataFrame:
     """preprocesses the data and adds features to the dataset
-
-    Args:
-        df (pd.DataFrame, optional): target df. Defaults to get_raw_dataset().
-        time_col (str, optional): name of the column with time data. Defaults to "created_on".
 
     Returns:
         pd.DataFrame: the prepared df
     """
+    df = get_raw_dataset()
     df = df.set_index("id")
+
     df = preprocess_data(df)
-    df = add_features(df, time_col)
+    df = add_features(df)
 
     return df
