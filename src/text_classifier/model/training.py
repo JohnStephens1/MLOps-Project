4fa8751816499b2f22da283f64e76618dd9a0f48
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-24 08:53:58 +0000

    chore: add random search fn

--- src/text_classifier/model/training.py
+++ src/text_classifier/model/training.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+import typing
+from sklearn.model_selection import cross_val_score, StratifiedKFold, RandomizedSearchCV
 from sklearn.pipeline import Pipeline
 
 
@@ -18,6 +19,17 @@ def get_cv_score(
         cv=get_cv_splitter(),
         scoring="f1_macro",
         verbose=1,
+    )
+
+def get_random_search(pipe: Pipeline, param_distribution: dict[str, typing.Any], X: pd.DataFrame, y: np.typing.ArrayLike):
+    return RandomizedSearchCV(
+        pipe,
+        param_distribution,
+        n_iter=100,
+        cv=get_cv_splitter(),
+        scoring="f1_macro",
+        random_state=42,
+        verbose=1
     )
 
 
